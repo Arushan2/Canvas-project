3c3af048d2a66f62f0ff6bbad1368d033dc92dc8
arushan2 <rockarush2@gmail.com>
2023-12-25 23:32:25 +0530

    add togglespan button and make it to work and drawing mode buttons only added

--- index.js
+++ index.js
@@ -12,7 +12,8 @@
 const initCanvas = (id) => {
     return new fabric.Canvas(id, {
         width: 500,
-        height: 500
+        height: 500,
+        Selection: false
     });
 };
 
@@ -22,31 +23,68 @@ const setBackground = (url, canvas) => {
         canvas.renderAll();
     });
 };
+const toggleMode= (mode) =>{
+    if(mode===modes.pan){
+        if(currentMode === modes.pan){
+            currentMode=''
+        } else {
+            currentMode = modes.pan
+        }
+    } else if (mode===modes.drawing){
+        if(currentMode === modes.drawing){
+            currentMode=''
+        } else {
+            currentMode = modes.drawing
+        }
+    }
+    console.log(mode)
+    
+}
+
+const setPanEvents =(canvas) =>{
+    // Event listeners for canvas interaction
+    canvas.on('mouse:move', (event) => {
+        if (mousePressed && currentMode===modes.pan) {
+            canvas.setCursor("grab")
+            canvas.renderAll()
+            const delta = new fabric.Point(event.e.movementX, event.e.movementY);
+            canvas.relativePan(delta);
+        }
+    });
+
+    canvas.on('mouse:down', () => {
+        mousePressed = true;
+        if(currentMode===modes.pan){
+            canvas.setCursor("grab")
+            canvas.renderAll()
+        }
+        canvas.setCursor("grab")
+        canvas.renderAll()
+    });
+
+    canvas.on('mouse:up', () => {
+        mousePressed = false;
+        canvas.setCursor("default")
+        canvas.renderAll()
+
+    });
+
+    // Event listener for the button to update the background image
+    document.getElementById('updateBackground').addEventListener('click', () => {
+        const imageUrl = document.getElementById('imageUrl').value;
+        setBackground(imageUrl, canvas);
+    });
+
+}
 
 const canvas = initCanvas('canvas');
 let mousePressed = false;
 
+let currentMode;
+const modes ={
+    pan:'pan',
+    drawing:'drawing'
+}
 // Initial background image
 setBackground('https://images.squarespace-cdn.com/content/v1/6298cb774cf3830bc9b342bf/1dcfee57-9a6e-49bb-9cd2-5088e88b946d/Hindu+Temple+Jaffna.jpg', canvas);
-
-// Event listeners for canvas interaction
-canvas.on('mouse:move', (event) => {
-    if (mousePressed) {
-        const delta = new fabric.Point(event.e.movementX, event.e.movementY);
-        canvas.relativePan(delta);
-    }
-});
-
-canvas.on('mouse:down', () => {
-    mousePressed = true;
-});
-
-canvas.on('mouse:up', () => {
-    mousePressed = false;
-});
-
-// Event listener for the button to update the background image
-document.getElementById('updateBackground').addEventListener('click', () => {
-    const imageUrl = document.getElementById('imageUrl').value;
-    setBackground(imageUrl, canvas);
-});
+setPanEvents(canvas)
